1e968a27bea64610070b21e9be28e6a159ecf50c
JKC <me@kunchen.me>
2020-04-01 00:48:01 -0400

    minor change

--- DataIngestion/extract_top_players.py
+++ DataIngestion/extract_top_players.py
@@ -18,14 +18,14 @@ def extract_top_players():
     leaderboard_url = BASE_URL + "leaderboards/division.bro.official.pc-2018-06/"
     modes = ['solo', 'duo', 'squad'] #, 'solo-fpp', 'duo-fpp', 'squad-fpp']
 
-    top_player_ids = set()
+    top_player_names = set()
 
     for mode in modes:
-        count = len(top_player_ids)
-        leaderboard_url_with_Mode = "{}{}?page[number]=0".format(leaderboard_url, mode)
+        count = len(top_player_names)
+        leaderboard_url_with_mode = leaderboard_url + "{}?page[number]=0".format(mode)
 
         # request data from api and check if the connect is successfully
-        rleader = requests.get(leaderboard_url_with_Mode, headers=API_HEADERS)
+        rleader = requests.get(leaderboard_url_with_mode, headers=API_HEADERS)
         if rleader.status_code == 200:
             print("Processing {}".format(mode))
         else:
@@ -41,15 +41,15 @@ def extract_top_players():
         player_objects = season_stat["included"]
 
         for player_object in player_objects:
-            top_player_ids.add(player_object["attributes"]["name"])
+            top_player_names.add(player_object["attributes"]["name"])
 
-        print("Added {} players in mode {}".format(len(top_player_ids) - count, mode))
+        print("Added {} players in mode {}".format(len(top_player_names) - count, mode))
 
-    print("Total {} unique players".format(len(top_player_ids)))
+    print("Total {} unique players found".format(len(top_player_names)))
 
     # We will be multiprocessing match lookup, so write to disk is an easier format to split loads
-    with open('player_names.txt', 'w+') as f:
-        for player_name in list(top_player_ids):
+    with open('player_names1.txt', 'w+') as f:
+        for player_name in list(top_player_names):
             f.write("{}\n".format(player_name))
 
 
