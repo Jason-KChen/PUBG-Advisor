54823be151753b753be8c95d9a51ccff24e6278a
nwan3 <nwan3@gatech.edu>
2020-04-04 19:45:41 -0400

    need to finish circle drawing

--- pubg_advisor_frontend/src/App.js
+++ pubg_advisor_frontend/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import React, {Component, useEffect} from 'react';
 import { TabView } from './tabs/TabView';
-import h337   from 'heatmap.js'
+import h337 from 'heatmap.js';
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import Dropdown from 'react-bootstrap/Dropdown'
-import HeatmapLayer from 'react-leaflet-heatmap-layer';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import { Map, ImageOverlay, TileLayer, Marker, Popup} from "react-leaflet";
+import L from 'leaflet';
 
 function getStyles() {
   return {
@@ -26,89 +26,114 @@ function getStyles() {
 const mapNames = ["Camp Jackal", "Erangel", "Karakin", "Miramar", "Sanhok", "Vikendi"];
 
 
-class MapContainer extends Component {
-  
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      currentMap: mapNames[0],
-    };
-  }
-  render() {
-    const buttonRef = React.createRef();
+class MapComponent extends Component {
     
 
-    function handleSelect(e) {
-      console.log(e);
-      this.setState(
-        (state, e) => {
-          return {currentMap: e};
+    constructor(props) {
+        super(props)
+        var fileName = this.getMapFileName(mapNames[0])
+        this.state = {
+            currentMap: mapNames[0],
+            currentMapFile: this.getMapFileName(mapNames[0])
+        };
+        // this.handleSelect = this.handleSelect.bind(this);
+    }
+
+    getMapFileName(mapName) {
+        var fileName =null;
+        if (mapName == "Camp Jackal") {
+            fileName = "Camp_Jackal";
+        } else {
+            fileName = mapName;
         }
-      );
+        return fileName;
     }
 
-    function ListItem(args) {
-      return <Dropdown.Item onSelect={() => handleSelect} eventKey={args.name}>{args.name}</Dropdown.Item>
+    handleSelect = (selectedMap) =>{
+        console.log(selectedMap);
+        var fileName = this.getMapFileName(selectedMap);
+
+        this.setState(
+            {currentMap: selectedMap,
+            currentMapFile: fileName
+            }
+        );
     }
 
-    return(
-      <div>
-        <DropdownButton id="dropdown-basic-button" title={this.state.currentMap} ref = {buttonRef}>
-            {mapNames.map((value, index) => {
-              return <ListItem key={index} name={value} />
-            })}
-        </DropdownButton>
-      </div>
-      );
-  }
+    componentDidMount() {
+    
 
+        // var heatmapInstance = h337.create({
+        //   // only container is required, the rest will be defaults
+        //   container: document.querySelector('.mapContainer')
+        // });
+        
+        var points = [];
+        var max = 0;
+        var width = 840;
+        var height = 400;
+        var len = 200;
+
+        while (len--) {
+          var val = Math.floor(Math.random()*100);
+          max = Math.max(max, val);
+          var point = {
+         x: Math.floor(Math.random()*width),
+         y: Math.floor(Math.random()*height),
+         value: val
+          };
+          points.push(point);
+        }
+        // heatmap data format
+        var data = {
+          max: max,
+          data: points
+        };
+        // if you have a set of datapoints always use setData instead of addData
+        // for data initialization
+        // heatmapInstance.setData(data);
+
+    }
+
+    
+    
+
+    render() {
+        const buttonRef = React.createRef();
+        const center = [0,0];
+        var bounds = [[0,0], [1000,1000]];
+
+        function ListItem(args) {
+            return <Dropdown.Item onSelect={args.handleSelect} eventKey={args.name}>{args.name}</Dropdown.Item>
+        }
+
+        return(
+            <div>
+            <DropdownButton id="dropdown-basic-button" title={this.state.currentMap} ref = {buttonRef}>
+                {mapNames.map((value, index) => {
+                    return <ListItem key={index} name={value} handleSelect={this.handleSelect}/>
+                })}
+            </DropdownButton>
+            <div className="mapContainer">
+                <img src={"maps/" + this.state.currentMapFile + ".png"}/>
+            </div>
+            </div>
+        );
+    
+    }
+    
 }
 
-
-class App extends Component{
+class App extends Component
+   {
   
 
-  componentDidMount() {
-    console.log('I am mounted!');
-    var heatmapInstance = h337.create({
-      // only container is required, the rest will be defaults
-      container: document.querySelector('.mapContainer')
-    });
-    // now generate some random data
-    var points = [];
-    var max = 0;
-    var width = 840;
-    var height = 400;
-    var len = 200;
-
-    while (len--) {
-      var val = Math.floor(Math.random()*100);
-      max = Math.max(max, val);
-      var point = {
-        x: Math.floor(Math.random()*width),
-        y: Math.floor(Math.random()*height),
-        value: val
-      };
-      points.push(point);
-    }
-    // heatmap data format
-    var data = {
-      max: max,
-      data: [{x: 10, y:33, value: 200},{x: 10, y:30, value: 2000},{x: 10, y:40, value: 2000},{x: 10, y:50, value: 2000}]
-    };
-    // if you have a set of datapoints always use setData instead of addData
-    // for data initialization
-    heatmapInstance.setData(data);
-  }
-  
   render() {
-     return (
-    <div className="mapContainer">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+
+    return (
+        <MapComponent/>   
+    );
   }
 }
 export default App;
