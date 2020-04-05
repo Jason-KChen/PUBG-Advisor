fb8feb4b06459926c145b9696a8a2d04a35e1e32
nwan3 <nwan3@gatech.edu>
2020-04-05 17:52:35 -0400

    need to add circle

--- pubg_advisor_frontend/src/App.js
+++ pubg_advisor_frontend/src/App.js
@@ -7,7 +7,7 @@ import HeatmapLayer from 'react-leaflet-heatmap-layer';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown';
 import { Map, ImageOverlay, TileLayer, Marker, Popup} from "react-leaflet";
-// import L from 'leaflet';
+import L from 'leaflet';
 
 function getStyles() {
   return {
@@ -37,10 +37,34 @@ class MapComponent extends Component {
         var fileName = this.getMapFileName(mapNames[0])
         this.state = {
             currentMap: mapNames[0],
-            currentMapFile: this.getMapFileName(mapNames[0])
+            currentMapFile: this.getMapFileName(mapNames[0]),
+            selectedPoint: null
         };
         // this.handleSelect = this.handleSelect.bind(this);
     }
+
+    // componentDidMount(){
+    //     var points = [];
+    //     var max = 0;
+    //     var width = 840;
+    //     var height = 400;
+    //     var len = 200;
+
+    //     while (len--) {
+    //       var val = Math.floor(Math.random()*100);
+    //       max = Math.max(max, val);
+    //       var point = [
+    //          Math.floor(Math.random()*width),
+    //          Math.floor(Math.random()*height)
+    //           ];
+    //       points.push(point);
+    //     }
+    //     // heatmap data format
+    //     var data = {
+    //       data: points
+    //     };
+    //     console.log(points);
+    // }
 
     getMapFileName(mapName) {
         var fileName =null;
@@ -52,34 +76,41 @@ class MapComponent extends Component {
         return fileName;
     }
 
-    handleSelect = (selectedMap) =>{
+    handleSelect = (selectedMap) => {
         console.log(selectedMap);
         var fileName = this.getMapFileName(selectedMap);
 
         this.setState(
             {currentMap: selectedMap,
-            currentMapFile: fileName
+            currentMapFile: fileName,
+            selectedPoint: this.state.selectedPoint
             }
         );
     }
 
-    componentDidMount() {
 
-        // var heatmapInstance = h337.create({
-        //   // only container is required, the rest will be defaults
-        //   container: document.querySelector('.mapContainer')
-        // });
-        
-        
-        // if you have a set of datapoints always use setData instead of addData
-        // for data initialization
-        // heatmapInstance.setData(data);
-        
-        // heatmapLayer.setData(data);
-        
+    userMapClick = (newPoint) => {
+        // console.log(newPoint);
+        // console.log(this.map.setView());
+        this.setState(
+            {currentMap: this.state.currentMap,
+            currentMapFile: this.state.currentMapFile,
+            selectedPoint: newPoint.latlng
+            }
+        );
     }
+    
+
 
     render() {
+        
+        const center = [0,0];
+        var b = 300;
+
+        function ListItem(args) {
+            return <Dropdown.Item onSelect={args.handleSelect} eventKey={args.name}>{args.name}</Dropdown.Item>
+        }
+
         var points = [];
         var max = 0;
         var width = 840;
@@ -95,18 +126,8 @@ class MapComponent extends Component {
               ];
           points.push(point);
         }
-        // heatmap data format
-        var data = {
-          data: points
-        };
-        console.log(points);
-        const center = [0,0];
-        var b = 300;
-
-        function ListItem(args) {
-            return <Dropdown.Item onSelect={args.handleSelect} eventKey={args.name}>{args.name}</Dropdown.Item>
-        }
-        // <img src={"maps/" + this.state.currentMapFile + ".png"}/>
+        
+     
         return(
             <div>
                 <DropdownButton id="dropdown-basic-button" title={this.state.currentMap} >
@@ -115,15 +136,24 @@ class MapComponent extends Component {
 
                     })}
                 </DropdownButton>
-                <Map center={[b/2,b/2]} zoom={4} ref={(ref) => { this.map = ref; }} setMaxBounds={[[0,0], [b,b]] } crs={Leaflet.CRS.Simple}>
+                <Map center={this.state.selectedPoint ? this.state.selectedPoint :[b/2,b/2]} zoom={2} id="physicalMap" ref={(ref) => { this.map = ref; }} setMaxBounds={[[0,0], [b,b]] } crs={Leaflet.CRS.Simple} onClick={this.userMapClick}>
                     <HeatmapLayer
                         fitBoundsOnLoad
                         fitBoundsOnUpdate
-                        points={points}
+                        points = {
+                            points
+                        }
                         longitudeExtractor={m => m[1]}
                         latitudeExtractor={m => m[0]}
                         intensityExtractor={m => parseFloat(m[2])} />
                     <ImageOverlay url={"maps/" + this.state.currentMapFile+ ".png"} bounds={[[0,0], [b,b]]}/>
+                    { this.state.selectedPoint !== null &&
+                        <Marker position={this.state.selectedPoint}>
+                            <Popup>
+                                <span>Popup</span>
+                            </Popup>
+                        </Marker>
+                    }
                 </Map>}
             </div>
         );
