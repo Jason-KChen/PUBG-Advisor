685e027f941e5415ea4bd75531ee578a1dc1a802
nwan3 <nwan3@gatech.edu>
2020-04-07 16:30:26 -0400

    moved code from app.js to HuntingLocationPanel

--- pubg_advisor_frontend/src/App.js
+++ pubg_advisor_frontend/src/App.js
@@ -1,157 +1,32 @@
 import './App.css';
-import React, {Component} from 'react';
-import HeatmapLayer from 'react-leaflet-heatmap-layer';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import Dropdown from 'react-bootstrap/Dropdown';
-import { Map, ImageOverlay, GridLayer, Marker, Popup, CircleMarker} from "react-leaflet";
-import L from 'leaflet';
+import React from 'react';
+import { TabView } from './tabs/TabView';
 
 function getStyles() {
-    return {
+  return {
     // Position the Panel in the middle of the screen
-      backgroundStyle: {
-        justifyContent: "center",
-        alignItems: "center"
+    backgroundStyle: {
+      justifyContent: "center",
+      alignItems: "center"
     },
     // Size of the Panel relative to the whole screen
-      tabViewWrapper: {
-        width: "70%",
-        height: "70%"
+    tabViewWrapper: {
+      width: "70%",
+      height: "70%"
     }
   }
 }
 
-const mapNames = ["Camp Jackal", "Erangel", "Karakin", "Miramar", "Sanhok", "Vikendi"];
+function App() {
+  let styles = getStyles();
 
-
-class MapComponent extends Component {
-    
-
-    constructor(props) {
-        super(props)
-        var fileName = this.getMapFileName(mapNames[0])
-        this.state = {
-            currentMap: mapNames[0],
-            currentMapFile: this.getMapFileName(mapNames[0]),
-            selectedPoint: null,
-            selectedRadius: null
-        };
-        // this.handleSelect = this.handleSelect.bind(this);
-        this.updateCircle = this.updateCircle.bind(this);
-    }
-
-    getMapFileName(mapName) {
-        var fileName =null;
-        if (mapName == "Camp Jackal") {
-            fileName = "Camp_Jackal";
-        } else {
-            fileName = mapName;
-        }
-        return fileName;
-    }
-
-    handleSelect = (selectedMap) => {
-        console.log(selectedMap);
-        var fileName = this.getMapFileName(selectedMap);
-
-        this.setState(
-            {currentMap: selectedMap,
-            currentMapFile: fileName,
-            selectedPoint: this.state.selectedPoint,
-            selectedRadius: this.state.selectedRadius
-            }
-        );
-    }
-
-
-    userMapClick = (newPoint) => {
-        this.setState(
-            {currentMap: this.state.currentMap,
-            currentMapFile: this.state.currentMapFile,
-            selectedPoint: newPoint.latlng,
-            selectedRadius: 20
-            }
-        );
-    }
-    
-    updateCircle(newRadius) {
-        console.log(newRadius);
-        this.setState(
-            {currentMap: this.state.currentMap,
-            currentMapFile: this.state.currentMapFile,
-            selectedPoint: this.state.selectedPoint,
-            selectedRadius: newRadius.target.value
-            }
-        );
-    }
-
-    render() {
-        
-        const center = [0,0];
-        var b = 300;
-
-        function ListItem(args) {
-            return <Dropdown.Item onSelect={args.handleSelect} eventKey={args.name}>{args.name}</Dropdown.Item>
-        }
-
-        var points = [];
-        
-     
-        return(
-
-            <div>
-                
-                <DropdownButton id="dropdown-basic-button" title={this.state.currentMap} >
-                    {mapNames.map((value, index) => {
-                        return <ListItem key={index} name={value} handleSelect={this.handleSelect}/>
-
-                    })}
-                </DropdownButton>
-                <Map center={this.state.selectedPoint ? this.state.selectedPoint :[b/2,b/2]} zoom={2} id="physicalMap" ref={(ref) => { this.map = ref; }} setMaxBounds={[[0,0], [b,b]] } crs={L.CRS.Simple} onClick={this.userMapClick}>
-                    <HeatmapLayer
-                        fitBoundsOnLoad
-                        fitBoundsOnUpdate
-                        points = {
-                            points
-                        }
-                        longitudeExtractor={m => m[1]}
-                        latitudeExtractor={m => m[0]}
-                        intensityExtractor={m => parseFloat(m[2])} />
-                    <ImageOverlay url={"maps/" + this.state.currentMapFile+ ".png"} bounds={[[0,0], [b,b]]}/>
-                    { 
-                        this.state.selectedPoint !== null &&
-                        <Marker position={this.state.selectedPoint}>
-                            <Popup>
-                                <form onSubmit={this.handleSubmit}>
-                                    <label>
-                                      Search Radius
-                                        <input value={this.state.selectedRadius} onChange={this.updateCircle} />
-                                    </label>
-                                    <input type="submit" value="Submit" />
-                                </form>
-                            </Popup>
-                        </Marker> 
-                    } 
-                    {
-                        this.state.selectedPoint !== null &&
-                        <CircleMarker center={this.state.selectedPoint} color="red" radius={this.state.selectedRadius}>
-                            <Popup>Popup in CircleMarker</Popup>
-                        </CircleMarker>
-                    }
-                </Map>}
-            </div>
-        );
-    
-    }
-    
+  return (
+    <div className="background col" style={styles.backgroundStyle}>
+      <div style={styles.tabViewWrapper}>
+        <TabView />
+      </div>
+    </div>
+  );
 }
 
-class App extends Component {
-  
-    render() {
-        return (
-            <MapComponent/>   
-        );
-    }
-}
 export default App;
--- pubg_advisor_frontend/src/tabs/HuntingLocationPanel.js
+++ pubg_advisor_frontend/src/tabs/HuntingLocationPanel.js
@@ -1,12 +1,140 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
+import HeatmapLayer from 'react-leaflet-heatmap-layer';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
+import { Map, ImageOverlay, GridLayer, Marker, Popup, CircleMarker} from "react-leaflet";
+import L from 'leaflet';
 
+const mapNames = ["Camp Jackal", "Erangel", "Karakin", "Miramar", "Sanhok", "Vikendi"];
+
+class MapComponent extends Component {
+    
+
+    constructor(props) {
+        super(props)
+        var fileName = this.getMapFileName(mapNames[0])
+        this.state = {
+            currentMap: mapNames[0],
+            currentMapFile: this.getMapFileName(mapNames[0]),
+            selectedPoint: null,
+            selectedRadius: null
+        };
+        // this.handleSelect = this.handleSelect.bind(this);
+        this.updateCircle = this.updateCircle.bind(this);
+    }
+
+    getMapFileName(mapName) {
+        var fileName =null;
+        if (mapName == "Camp Jackal") {
+            fileName = "Camp_Jackal";
+        } else {
+            fileName = mapName;
+        }
+        return fileName;
+    }
+
+    handleSelect = (selectedMap) => {
+        console.log(selectedMap);
+        var fileName = this.getMapFileName(selectedMap);
+
+        this.setState(
+            {currentMap: selectedMap,
+            currentMapFile: fileName,
+            selectedPoint: this.state.selectedPoint,
+            selectedRadius: this.state.selectedRadius
+            }
+        );
+    }
+
+
+    userMapClick = (newPoint) => {
+        this.setState(
+            {currentMap: this.state.currentMap,
+            currentMapFile: this.state.currentMapFile,
+            selectedPoint: newPoint.latlng,
+            selectedRadius: 20
+            }
+        );
+    }
+    
+    updateCircle(newRadius) {
+        console.log(newRadius);
+        this.setState(
+            {currentMap: this.state.currentMap,
+            currentMapFile: this.state.currentMapFile,
+            selectedPoint: this.state.selectedPoint,
+            selectedRadius: newRadius.target.value
+            }
+        );
+    }
+
+    render() {
+        
+        const center = [0,0];
+        var b = 300;
+
+        function ListItem(args) {
+            return <Dropdown.Item onSelect={args.handleSelect} eventKey={args.name}>{args.name}</Dropdown.Item>
+        }
+
+        var points = [];
+        
+     
+        return(
+
+            <div>
+                
+                <DropdownButton id="dropdown-basic-button" title={this.state.currentMap} >
+                    {mapNames.map((value, index) => {
+                        return <ListItem key={index} name={value} handleSelect={this.handleSelect}/>
+
+                    })}
+                </DropdownButton>
+                <Map center={this.state.selectedPoint ? this.state.selectedPoint :[b/2,b/2]} zoom={2} id="physicalMap" ref={(ref) => { this.map = ref; }} setMaxBounds={[[0,0], [b,b]] } crs={L.CRS.Simple} onClick={this.userMapClick}>
+                    <HeatmapLayer
+                        fitBoundsOnLoad
+                        fitBoundsOnUpdate
+                        points = {
+                            points
+                        }
+                        longitudeExtractor={m => m[1]}
+                        latitudeExtractor={m => m[0]}
+                        intensityExtractor={m => parseFloat(m[2])} />
+                    <ImageOverlay url={"maps/" + this.state.currentMapFile+ ".png"} bounds={[[0,0], [b,b]]}/>
+                    { 
+                        this.state.selectedPoint !== null &&
+                        <Marker position={this.state.selectedPoint}>
+                            <Popup>
+                                <form onSubmit={this.handleSubmit}>
+                                    <label>
+                                      Search Radius
+                                        <input value={this.state.selectedRadius} onChange={this.updateCircle} />
+                                    </label>
+                                    <input type="submit" value="Submit" />
+                                </form>
+                            </Popup>
+                        </Marker> 
+                    } 
+                    {
+                        this.state.selectedPoint !== null &&
+                        <CircleMarker center={this.state.selectedPoint} color="red" radius={this.state.selectedRadius}>
+                            <Popup>Popup in CircleMarker</Popup>
+                        </CircleMarker>
+                    }
+                </Map>}
+            </div>
+        );
+    
+    }
+    
+}
 class HuntingLocationPanel extends React.Component {
   render() {
     return (
       <div className="col panel-layout">
         <Typography>
-          Interactive Heatmap here
+          <MapComponent/>
         </Typography>
       </div>
     );
